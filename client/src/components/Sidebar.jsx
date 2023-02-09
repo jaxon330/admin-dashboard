3909a261e77282834d8a8beee8774643021be594
john-330 <jaxon330@gmail.com>
2023-02-09 16:20:45 -0600

    working on Homepage

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -8,7 +8,13 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
@@ -35,7 +41,7 @@ const Sidebar = () => {
                 <NavLink 
                   to={`/${link.name}`} 
                   key={link.name} 
-                  onClick={() => {}}
+                  onClick={handleCloseSideBar}
                   className={({ isActive }) => isActive ? activeLink : normalLink }
                 >
                   {link.icon}
